6870fb9d0289f2614a58555053446839fe91032c
s150928 <s150928@berufskolleg-borken.de>
2019-02-18 12:45:28 +0100

    ...

--- server.js
+++ server.js
@@ -18,18 +18,23 @@ const app = express()
 
 app.set('view engine', 'ejs')
 
-// ...
+// definiert die View engine 
+// ejs muss noch installiert werden (schon im Unterricht gemacht)
 
 app.use(express.static('public'))
 
-// ...
+// der App mitgeteilt wo die statischen Befehle liegen
 
 const bodyParser = require('body-parser')
 
-// ...
+// Darf sich nicht ändern
+// Zeichenketten in Hochkomma
+// Modul: body parser
+// bereitet die Daten aus dem html Formular für die Übergabe an server.js vor
+// node pa
 
 app.use(bodyParser.urlencoded({extended: true}))
 
-// ...
+// Bodypraser wird in die App eingebunden
 
 const server = app.listen(process.env.PORT || 3000, () => {        console.log('Server lauscht auf Port %s', server.address().port)    })
