76b63a908014ad66d9dd05c738e4595a3d13917b
s150928 <s150928@berufskolleg-borken.de>
2019-02-18 14:29:31 +0100

    ...

--- server.js
+++ server.js
@@ -6,10 +6,18 @@
 // Das framework muss mit npm installiert werden:
 // im Terminal: npm install express --save
 
+const bodyParser = require('body-parser')
+
+// Darf sich nicht ändern
+// Zeichenketten in Hochkomma
+// Modul: body parser
+// bereitet die Daten aus dem html Formular für die Übergabe an server.js vor
+// npm muss runtergeladen werden (schon installiert)
+
 const express = require('express')
 
 // Das app-Objekt wird initialisiert
-// Das spp-Objekt repräsentiert den Server.
+// Das app-Objekt repräsentiert den Server.
 // Auf das app-Objekt werden im folgenden Methoden aufgerufen.
 
 const app = express()
@@ -25,16 +33,22 @@ app.use(express.static('public'))
 
 // der App mitgeteilt wo die statischen Befehle liegen
 
-const bodyParser = require('body-parser')
-
-// Darf sich nicht ändern
-// Zeichenketten in Hochkomma
-// Modul: body parser
-// bereitet die Daten aus dem html Formular für die Übergabe an server.js vor
-// node pa
-
 app.use(bodyParser.urlencoded({extended: true}))
 
 // Bodypraser wird in die App eingebunden
 
-const server = app.listen(process.env.PORT || 3000, () => {        console.log('Server lauscht auf Port %s', server.address().port)    })
+const server = app.listen(process.env.PORT || 3000, () => {
+    console.log('Server lauscht auf Port %s', server.address().port)    
+})
+
+// Der server läuft auf einem bestimmten Port
+// Localhost ist das eigene Gerät
+// console.log Möglichkeit sich den Erfolg das laufenden Server auf der Konsole loggen zu lassen
+// mit dem Befehl node .\server.js in dem Terminal kann man den Server starten, mit strg+c kann man diesen wieder stoppen
+
+app.get('/',(req, res, next) => {
+    res.render('index.ejs', {
+
+    })
+})
+
