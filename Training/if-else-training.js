e83c12f8b4304d3bfa1bd81d922f423fb6224a92
s150928 <s150928@berufskolleg-borken.de>
2019-03-25 15:30:14 +0100

    ...

--- Training/if-else-training.js
+++ Training/if-else-training.js
@@ -8,64 +8,64 @@
  */
 
 // Deklaration und Initialisierung 
-// Lies: "der Wert 0 wird zugewiesen an eine Variable namens x"
- let x = 0
- let y = 1
+// Lies: "Der Wert 0 wird zugewiesen an
+//        eine Variable namens x"
+ 
+let x = 0
+let y = 1
 
- if("GW11A".endsWith("A") || "GW11A".endsWith("B") || "GW11A".startsWith("GT")){
-    console.log("Die Prüfung ist WAHR. Es werden die Anweisungen im Rumpf von IF ausgeführt")
- }else{
-    console.log("Die Prüfung ist FALSCH. Es werden die Anweisungen im Rumpf von ELSE ausgeführt")
- }
+if("GW11A".endsWith("A")||"GW11A".endsWith("B")||"GW11A".startsWith("GT")){
+    console.log("Die Prüfung ist WAHR. Es werden die Anweisungen im Rumpf von IF ausgeführt.")
+}else{
+    console.log("Die Prüfung ist FALSCH. Es werden die Anweisungen im Rumpf von ELSE ausgeführt.")
+}
 
- // FÄLLE: 
 
- // 22. if()
+// FÄLLE:
 
- // 21. if("GW11A".endsWith("A"))   Der Ausdruck ist WAHR. Die Funktion prüft, ob eine Zeichenkette mit einer anderen Zeichenkette endet.
+// 22. if()
 
- // 20. if("GW11A".includes("11"))  Der Ausdruck ist WAHR. Die Funktion prüft, ob eine Zeichenkette mit einer andere Zeichenkette enthält.
+// 21 if ("Gw11a".endsWith("a")) Der Ausdruck ist Wahr. Die Funktion prüft, ob eine Zeichenkette mit einer anderen endet.
 
- // 19. if("GW11A".startsWith("G")) Der Ausdruck ist WAHR. Die Funktion prüft, ob eine Zeichenkette mit einer anderen Zeichenkette beginnt.
+// 20. if("GW11A".includes("11")) Der Ausdruck ist Wahr. Die Funktion prüft, ob eine Zeichenkette eine andere Zeichenkette enthält.
 
- // 18. if("Text".length === 4)     Der Ausdruck ist WAHR. Die Eigenschaft .length gibt die Anzahl der Zeichen einer Zeichenkette zurück.
+// 19. if("GW11A".startsWith("G")) Der Ausdruck ist Wahr. Die Funktion prüft, ob eine Zeichenkette mit einer anderen Zeichenkette beginnt.
 
- // 17. if(undefined)               Der Ausdruck ist FALSCH. Ein Objekt ist "undefinded", wenn sie nicht existiert.
+// 18. if("GW11A".length === 5) Der Ausdruck ist wahr. Die Eigenschaft length gibt die Anzahl der Zeichen einer Zeichenkette zurück.
 
- // 16. if(1 > 2 || 1 == 1)         Der Ausdruck ist WAHR, weil mindestens eine Prüfung WAHR ist.
+// 17. if(undefined)    Der Ausdruck ist falsch. Ein Objekt ist undefined, wenn sie nicht existiert.
 
- // 15. if(1 > 2 && 1 == 1)         Der Ausdruck ist FALSCH, weil die erste Prüfung FALSCH ist.
- //                 Alle mit && verketteten Prüfungen müssen WAHR sein, damit die Prüfung insgesamt WAHR wird.
+// 16. if(1 > 2 || 1 == 1) Der Ausdruck ist wahr, weil eine der beiden Prüfungen wahr ist.
+// 15. if(1 > 2 && 1 == 1) Der Ausdruck ist Falsch, weil die erste Prüfung Falsch ist.Alle mit && verketteten Prüfungen müssen Wahr sein, damit die Prüfung insgesamt Wahr wird.
+// 14. if(false)      Der Ausdruck ist falsch. 
 
- // 14. if(false)                   Der Ausdruck ist FALSCH.
+// 13. if (true)    Der Ausdruck ist wahr.
 
- // 13. if(true)                    Der Ausdruck ist WAHR.
+// 12. if (0)      Die Zahl 0 ist falsch.
 
- // 12. if(0)                       Die Zahl 0 ist FALSCH.
+// 11. if (-123)   Zahlen sind wahr bis auf 0.
 
- // 11. if(69)                      Zahlen sind WAHR, außer 0.
+// 10. if("")      leere Zeichenketten in javascript sind falsch.
 
- // 10. if("")                      leere Zeichenketten in Javascript sind FALSCH
+// 9. if("ah")     nicht leere Zeichenketten in javascript sind wahr.
 
- // 9. if("Text")                   nicht leere Zeichenketten in Javascript sind WAHR.
+//8. if(1 != 0)     Vergleichsoperator: "ungleich"       Der Ausdruck ist wahr.
 
- // 8. if(1 != 0)                   Vergleichsoperator: "ungleich"          Der Ausdruck ist WAHR.
+// 7. if(1 <= 1)    Vergleichsoperator: "kleiner gleich"    Der Ausdruck ist wahr.
 
- // 7. if(1 <= 1)                   Vergleichsoperator: "kleiner gleich"    Der Ausdruck ist WAHR.
+// 6. if(1 >= 1)    Vergleichsoperator: "größer gleich"   Der Ausdruck ist wahr.
 
- // 6. if(1 >= 1)                   Vergleichsoperator: "größer gleich"     Der Ausdruck ist WAHR.
+// 5. if(1 === "1") Vergleichsoperator: "gleich"          Der Ausdruck ist falsch.
+//                  Das 3fache "===" prüft auf Gleichheit des Wertes und des Typs.
+//                  "1" : String / Zeichenkette
+//                   1  : Zahl 
 
- // 5. if(1 === "1")                Vergeleichsoperator: "gleich"           Der Ausdruck ist FALSCH.
- //                 Das 3fache "===" prüft auf Gleichheit des Wertes und des Typs.
- //                 "1" :   Stringn / Zechenkette
- //                 1   :   Zahl
+// 4. if(1 == "1")  Vergleichsoperator: "gleich"          Der Ausdruck ist wahr.
+//                  Das doppelte "==" prüft auf Gleichheit des Wertes,
+//                  aber nicht auf Gleichheit des Typs.
 
- // 4. if(1 == "1")                 Vergleichsoperator: "gleich"            Der Ausdruck ist WAHR.
- //                 Das Doppelte "==" prüft auf Gleichheit des Wertes,
- //                 aber nicht auf Gleichheit des Typs.
+// 3. if(0 == 1)    Vergleichsoperator: "gleich"          Der Ausdruck ist falsch.
 
- // 3. if(0 == 1)                   Vergleichsoperator: "gleich"            Der Ausdruck ist FALSCH.
+// 2. if(0 > 1)      Vergleichsoperator: "größer als"     Der Ausdruck ist falsch.
 
- // 2. if(0 > 1)                    Vergleichsoperator: "größer als"        Der Ausdruck ist FALSCH.
-
- // 1. if(0 < 1)                    Vergleichsoperator: "kleiner als"       Der Ausdruck ist WAHR.
+// 1. if(0 < 1)      Vergleichsoperator: "kleiner als"    Der Ausdruck ist wahr.
--- server.js
+++ server.js
@@ -1,65 +1,69 @@
-// Das ist ein Einzeiliger Kommentar
-/* Das ist ein Mehrzeiliger Kommentar*/
-
-// Das express-framework wird eingebunden.
-// Ein framework soll die Programierung erleichtern.
-// Das framework muss mit npm installiert werden:
-// im Terminal: npm install express --save
-
-const bodyParser = require('body-parser')
-
-// Darf sich nicht ändern
-// Zeichenketten in Hochkomma
-// Modul: body parser
-// bereitet die Daten aus dem html Formular für die Übergabe an server.js vor
-// npm muss runtergeladen werden (schon installiert)
-
 const express = require('express')
-
-// Das app-Objekt wird initialisiert
-// Das app-Objekt repräsentiert den Server.
-// Auf das app-Objekt werden im folgenden Methoden aufgerufen.
-
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const app = express()
-
-// Mit der ejs-View-Engine werden Werte von der server.js zur index-Datei gegeben.
-
 app.set('view engine', 'ejs')
-
-// definiert die View engine 
-// ejs muss noch installiert werden (schon im Unterricht gemacht)
-
 app.use(express.static('public'))
-
-// der App mitgeteilt wo die statischen Befehle liegen
-
 app.use(bodyParser.urlencoded({extended: true}))
-
-// Bodypraser wird in die App eingebunden
+app.use(cookieParser())
 
 const server = app.listen(process.env.PORT || 3000, () => {
     console.log('Server lauscht auf Port %s', server.address().port)    
 })
 
-// Der server läuft auf einem bestimmten Port
-// Localhost ist das eigene Gerät
-// console.log Möglichkeit sich den Erfolg das laufenden Server auf der Konsole loggen zu lassen
-// mit dem Befehl node .\server.js in dem Terminal kann man den Server starten, mit strg+c kann man diesen wieder stoppen
+app.get('/',(req, res, next) => {   
 
-app.get('/',(req, res, next) => {
-
+    let idKunde = req.cookies['istAngemeldetAls']
     
-    1+(req.cookies['istAngemeldetAls']){
-
+    if(idKunde){
+        console.log("Kunde ist angemeldet als " + idKunde)
+        res.render('index.ejs', {                              
+        })
+    }else{
+        res.render('login.ejs', {                    
+        })    
     }
+})
 
-    res.render('index.ejs', {
+// Wenn die Seite localhost:3000/impressum aufgerufen wird,...
 
+app.get('/impressum',(req, res, next) => {   
+
+    let idKunde = req.cookies['istAngemeldetAls']
+    
+    if(idKunde){
+        console.log("Kunde ist angemeldet als " + idKunde)
+        
+        //... dann wird impressum.ejs gerendert.
+
+        res.render('impressum.ejs', {                              
+        })
+    }else{
+        res.render('login.ejs', {                    
+        })    
+    }
+})
+
+app.get('/login',(req, res, next) => {         
+    res.cookie('istAngemeldetAls', '')       
+    res.render('login.ejs', {                    
     })
 })
 
-app.get('/login',(req,res,next)->{
-    res.reader('login.ejs',(
-    })
-
-
+app.post('/',(req, res, next) => {   
+    
+    const idKunde = req.body.idKunde
+    const kennwort = req.body.kennwort
+        
+    if(idKunde === "4711" && kennwort === "123"){            
+        console.log("Der Cookie wird gesetzt:")
+        res.cookie('istAngemeldetAls', idKunde)
+        res.render('index.ejs', {           
+        })
+    }else{            
+        console.log("Der Cookie wird gelöscht")
+        res.cookie('istAngemeldetAls','')
+        res.render('login.ejs', {                    
+        })
+    }
+})
